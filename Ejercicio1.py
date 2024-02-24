c2d9420beb0d3f0e1476f70eb39dda1e33d977e6
sortiz670 <sortiz670@soyudemedellin.edu.co>
2024-02-23 22:07:32 -0500

    primer ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+#Cree una clase Vehículo que contenga los atributos de instancia velocidad_maxima y kilometraje.
+
+class Vehiculo:
+  def __init__(self, velocidad_maxima, kilometraje):
+    self.velocidad_maxima = velocidad_maxima
+    self.kilometraje = kilometraje
+mi_vehiculo = Vehiculo(200, 50000)
+print("Velocidad maxima:",mi_vehiculo.velocidad_maxima)
+mi_vehiculo.velocidad_maxima
+print("Kilometraje:",mi_vehiculo.kilometraje)
