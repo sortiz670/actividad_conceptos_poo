119652f6c7553b07ccd97533d8d14a4dacbdf4a8
sortiz670 <sortiz670@soyudemedellin.edu.co>
2024-02-23 23:50:52 -0500

    primer ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -15,7 +15,6 @@ class Circulo:
         distancia = ((punto[0] - self.centro[0]) ** 2 + (punto[1] - self.centro[1]) ** 2) ** 0.5
         return distancia <= self.radio
 
-# Ejemplo de uso
 circulo = Circulo((0, 0), 5)
 
 print("Área:", circulo.area())
